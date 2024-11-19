ff76b9d372916e6aee5439566124887d5f1c9277
Max <58192471+DiMister@users.noreply.github.com>
2024-11-19 12:07:53 -0600

    Update IMU to be simlar to rest of code

--- Subsystems/IMU.py
+++ Subsystems/IMU.py
@@ -4,29 +4,30 @@ import busio
 from adafruit_lsm6ds.lsm6ds33 import LSM6DS33
 from Subsystem import Subsystem
 
-#INCLUDE THIS IN MAIN FUNCTION
-i2c = busio.I2C(board.SCL, board.SDA)
-sensor = LSM6DS33(i2c)
+
 
 class IMU(Subsystem):
     
     def __init__(self):
+        #INCLUDE THIS IN MAIN FUNCTION
+        i2c = busio.I2C(board.SCL, board.SDA)
+        self.sensor = LSM6DS33(i2c)
 
         self.acceleration = []
         self.gyro = []
 
     def periodic(self):
         #FOR DEBUG
-        print("Acceleration: X:%.2f, Y: %.2f, Z: %.2f m/s^2" % (sensor.acceleration))
-        print("Gyro X:%.2f, Y: %.2f, Z: %.2f radians/s" % (sensor.gyro))
+        print("Acceleration: X:%.2f, Y: %.2f, Z: %.2f m/s^2" % (self.sensor.acceleration))
+        print("Gyro X:%.2f, Y: %.2f, Z: %.2f radians/s" % (self.sensor.gyro))
         
         #destroys info in array (hopefully)
         self.gyro.pop[0,1,2]
         self.acceleration.pop[0,1,2]
         
         #adds new info into array
-        self.gyro.append [sensor.acceleration]
-        self.acceleration.append[sensor.gyro]
+        self.gyro.append [self.sensor.acceleration]
+        self.acceleration.append[self.sensor.gyro]
         
         #(DEBUG) prints blank line for formatting
         print("")
