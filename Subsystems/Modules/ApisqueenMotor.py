2aa695d71737ea47f92c1c0597fe6e6233dc37c5
Sawyer <sawyer.c.theis@gmail.com>
2025-01-27 18:34:22 -0600

    refactor: adjust power mapping for ApisqueenMotor to improve motor control

--- Subsystems/Modules/ApisqueenMotor.py
+++ Subsystems/Modules/ApisqueenMotor.py
@@ -16,7 +16,11 @@ class ApisqueenMotor:
     def set_power(self, power):
         # pulse_width is in microseconds
         # pulse_width should be between 1000 and 2000
-        self.motor.value = power
+        
+        # map power from -1 to 1 to 0.05 to 0.1
+        map_power = (power + 1) / 2 * 0.025 + 0.05
+        
+        self.motor.value = map_power
 
     def stop(self):
         self.set_power(0.075)
