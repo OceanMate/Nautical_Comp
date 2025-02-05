9172d9c7ed395e632908085b464538af682a1531
Max <58192471+DiMister@users.noreply.github.com>
2025-02-05 15:28:49 -0600

    basic acceleration smoothing code

--- Subsystems/Modules/BilgePumpMotor.py
+++ Subsystems/Modules/BilgePumpMotor.py
@@ -1,6 +1,7 @@
 from gpiozero import LED
 from gpiozero import PWMLED
 from Constants import Constants
+import time
 
 
 class BilgePumpMotor:
@@ -15,11 +16,31 @@ class BilgePumpMotor:
         self.backward = PWMLED(self.backwardID)
         self.speed = PWMLED(self.speedID)
         
+        # acceleration smoothing variables
+        self.actualPower = 0
+        self.time_between_steps = 0.1
+        self.lastStep = time.time()
+        
     
-
+    def update(self):
+        # acceleration smoothing for the motor
+        # using time to update based on time not cycles
+        if(time.time() - self.lastStep >= self.time_between_steps):
+            
+            if(self.actualPower < self.desiredPower):
+                self.actualPower += 0.01
+            elif(self.actualPower > self.desiredPower):
+                self.actualPower -= 0.01
+            self.lastStep = time.time()
+            self.set_power_real(self.actualPower)
+        pass
+    
+    def set_power(self, power):
+        power = Constants.clamp(power,0,1)
+        self.desiredPower = power
 
     # input power is a number between -1 and 1
-    def set_power(self, power):
+    def set_power_real(self, power):
         # set the direction of the motor
         if power < 0:
             self.forward.value = 0
