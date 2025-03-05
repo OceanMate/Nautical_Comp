fe79d15acb9ee4e3e76a0bf0d7037bb0bb8ae53f
Max <58192471+DiMister@users.noreply.github.com>
2025-03-04 19:09:59 -0600

    stuff

--- Constants.py
+++ Constants.py
@@ -1,14 +1,14 @@
 
 class Constants:
     # vertical motor pins
-    frontVerticalMotorPin = 0
-    backVerticalMotorPin = 1
+    frontVerticalMotorPin = 4
+    backVerticalMotorPin = 5
 
     # horizontal motor pins
-    frontLeftMotorPin = 2
-    frontRightMotorPin = 3
-    backLeftMotorPin = 4
-    backRightMotorPin = 5
+    frontLeftMotorPin = 0
+    frontRightMotorPin = 1
+    backLeftMotorPin = 2
+    backRightMotorPin = 3
 
     # math scaling function 
     def map(x: float, in_min: float, in_max: float, out_min: float, out_max: float) -> float:
