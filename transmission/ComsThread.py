b7343a10005a980360990988ce35c191f1ef418c
Sawyer <sawyer.c.theis@gmail.com>
2025-03-18 19:40:44 -0500

    Add filtering for available network interfaces in ComsThread; exclude link-local addresses

--- transmission/ComsThread.py
+++ transmission/ComsThread.py
@@ -36,6 +36,15 @@ class ComsThread:
         try:
             addresses = psutil.net_if_addrs()
             stats = psutil.net_if_stats()
+            
+            available_networks = []
+            for intface, addr_list in addresses.items():
+                if any(getattr(addr, 'address').startswith("169.254") for addr in addr_list):
+                    continue
+                elif intface in stats and getattr(stats[intface], "isup"):
+                    available_networks.append(intface)
+
+            print(available_networks)
 
             for intface, addr_list in addresses.items():
                 # Check if the interface is up and contains 'Ethernet' in its name
