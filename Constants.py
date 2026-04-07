6c0f1b9f31a94304bc02b8fc0a1180ed297eff3a
Max <58192471+DiMister@users.noreply.github.com>
2026-04-06 20:11:22 -0500

    water pin new

--- Constants.py
+++ Constants.py
@@ -19,7 +19,7 @@ class Constants:
     clawClampMotorPin = 15
 
     #watersensor pins
-    waterSensorPin = 13
+    waterSensorPin = 4
     
     i2c = busio.I2C(board.SCL, board.SDA)
     pca = adafruit_pca9685.PCA9685(i2c)
